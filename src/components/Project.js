36fac0f3fca9fc6bf5c424f72011847888fb2ba7
renusushmak <sushmakr533@gmail.com>
2021-08-29 22:51:26 -0700

    add projects portfolio

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Project({title, image, link}) {
+function Project({title, image, link, dlink}) {
     return(
       <div>
         <h1>{title}</h1>
         <img src = {image}></img>
-        <a href = {link}> github link</a>
+        <a href = {link}>Github link</a><br></br>
+        <a href = {dlink}>Deployed link</a>
       </div>
     )
 }
